b27234039fbd38827592272e0fe6fcc294f1472a
Cerberose <tranlethaison123@gmail.com>
2022-11-04 08:32:43 +0700

    update

---
+++ pages/api/socket.js
@@ -0,0 +1,41 @@
+import { Server } from 'Socket.IO'
+
+const SocketHandler = (req, res) => {
+  if (res.socket.server.io) {
+    console.log('Socket is already running')
+  } else {
+    let players = [];
+    let turn = 0;
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
+
+    io.on('connection', socket => {
+      socket.on('player-check', () => {
+        socket.emit('start-game', players.length);
+        socket.broadcast.emit('start-game', players.length);
+      });
+      players.push(socket.id);
+      console.log(players);
+      socket.on('input-change', msg => {
+        if (socket.id === players[turn % 2]) {
+          socket.broadcast.emit('update-input', msg);
+          socket.emit('update-input', msg);
+          turn++;
+        }
+      });
+      socket.on('game-restart', msg => {
+        socket.broadcast.emit('update-input', msg);
+      })
+      socket.on('disconnect', () => {
+        socket.broadcast.emit('player-disconnect', 'Player Disconnected');
+        players.splice(players.indexOf(socket),1);
+        turn = 0;
+        console.log(players);
+      });
+    })
+  }
+  res.end()
+}
+
+export default SocketHandler;
